fb9cd06d60e2dae4af85ed9eeab4f9b87da0e843
Vishal Jain <kunwarvjain@gmail.com>
2020-03-13 20:10:12 -0400

    generalize target name, same name bin and lib
    Generalized target names in such a way that both Binary and
    Library/shared object produced can have same name.

--- otto_run.cmake
+++ otto_run.cmake
@@ -9,7 +9,11 @@ otto_create_version()
 
 ## If binary is required, add target
 if (APP_NAME)
-    add_executable(${APP_NAME} ${APP_OBJS})
+    add_executable(${APP_NAME}-bin ${APP_OBJS})
+    set_target_properties(${APP_NAME}-bin
+        PROPERTIES
+            OUTPUT_NAME ${APP_NAME}
+    )
 endif()
 
 ## If shared library is required, add target
@@ -19,10 +23,16 @@ if (LIB_NAME)
         PROPERTIES
             VERSION ${PROJECT_VERSION}
             SOVERSION ${PROJECT_VERSION_MAJOR}
+    )
+    if (LIB_HEADERS)
+        set_target_properties(${LIB_NAME}
+            PROPERTIES
+                PUBLIC_HEADER ${LIB_HEADERS}
         )
+    endif()
 
     ## If shared library and binary are required, link binary
     if (APP_NAME)
-        target_link_libraries(${APP_NAME} ${LIB_NAME})
+        target_link_libraries(${APP_NAME}-bin ${LIB_NAME})
     endif()
 endif()
