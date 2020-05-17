df80b3ceea9ed9a3e7a3e1fd36ce7970531b7a66
Vishal Jain <kunwarvjain@gmail.com>
2020-05-16 21:50:09 -0400

    set project name as target

--- otto_install.cmake
+++ otto_install.cmake
@@ -1,5 +1,5 @@
 if (LIB_NAME)
-    install(TARGETS ${LIB_NAME} EXPORT ${LIB_NAME}Targets
+    install(TARGETS ${LIB_NAME} EXPORT ${PROJECT_NAME}Targets
         LIBRARY DESTINATION ${OTTO_PROJECT_INSTALL_PREFIX}/lib$<$<CONFIG:Debug>:_debug>
         PUBLIC_HEADER DESTINATION ${OTTO_PROJECT_INSTALL_PREFIX}/include
     )
@@ -7,7 +7,7 @@ if (LIB_NAME)
     include(otto_find)
 endif()
 if (APP_NAME)
-    install(TARGETS ${APP_NAME}-bin EXPORT ${APP_NAME}Targets
+    install(TARGETS ${APP_NAME}-bin EXPORT ${PROJECT_NAME}Targets
         RUNTIME DESTINATION ${OTTO_PROJECT_INSTALL_PREFIX}/bin$<$<CONFIG:Debug>:_debug>
     )
 endif()
